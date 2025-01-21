4136d2f4ac8c2d5787b10b53e25c7120f7e2d0bf
shushaykebedew <shushaykebedew12@gmail.com>
2025-01-21 23:12:46 +0300

    hero section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Hero/Hero";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
